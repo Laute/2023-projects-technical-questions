0c59b9ce8ef2332ee206622ef90b770073b154d5
Laute <52260930+Laute@users.noreply.github.com>
2023-01-16 20:59:09 +1100

    Complete q2 part 2

--- q2-backend/ts_template/roundupper_100.ts
+++ q2-backend/ts_template/roundupper_100.ts
@@ -31,9 +31,34 @@ app.post("/entity", (req, res) => {
   res.status(200).json({});
 });
 
-// /lassoable returns all the space animals a space cowboy can lasso given their name
+// returns all space animals a space cowboy can lasso given their name
 app.get("/lassoable", (req, res) => {
-  // TODO: fill me in
+  // TODO: validate input
+  const name = req.query.cowboy_name as string;
+  const cowboys = spaceEntities.filter(
+    (entity): entity is SpaceCowboy => entity.type === "space_cowboy"
+  );
+  const cowboy = cowboys.find((cowboy) => cowboy.metadata.name === name) as SpaceCowboy; // FIXME: narrow type instead by validating input
+
+  const animals = spaceEntities.filter(
+    (entity): entity is SpaceAnimal => entity.type === "space_animal"
+  );
+  const lassoableAnimals = animals.filter(
+    (animal) => distance(cowboy.location, animal.location) <= cowboy.metadata.lassoLength
+  );
+
+  res.status(200).json({
+    space_animals: lassoableAnimals.map((animal) => ({
+      type: animal.metadata.type,
+      location: animal.location,
+    })),
+  });
 });
 
 app.listen(8080);
+
+function distance(location1: Location, location2: Location) {
+  const xDifference = location1.x - location2.x;
+  const yDifference = location1.y - location2.y;
+  return Math.sqrt(Math.pow(xDifference, 2) + Math.pow(yDifference, 2));
+}
